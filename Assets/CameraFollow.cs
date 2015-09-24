a5d04342771fc0d53acf40d8ef307d78a4e249dd
elborio <elborio@hotmail.com>
2015-09-25 01:58:00 +0200

    Sign bij minigame en huisje erbij

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public GameObject target;
+	public float followDistance;
+	public float followHeight;
+	public float speed;
+
+	void FixedUpdate()
+	{
+
+		Vector3 targetPosition = target.transform.position + new Vector3(0,followHeight,followDistance);
+		targetPosition = Vector3.Lerp(transform.position,targetPosition,Time.deltaTime*speed);
+		transform.position = targetPosition;
+		transform.LookAt(target.transform.position);
+	}
+}
