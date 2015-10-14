4066b4301d15952344fc237c559b369ab4625365
elborio <elborio@hotmail.com>
2015-10-15 00:04:29 +0200

    Do t pls

--- Assets/Scripts/MiniGames/2e Minigame/Enemy1.cs
+++ Assets/Scripts/MiniGames/2e Minigame/Enemy1.cs
@@ -4,49 +4,14 @@ using System.Collections;
 public class Enemy1 : MonoBehaviour {
     public NavMeshAgent nma;
     public GameObject player;
-    private bool asked = false;
-    private Question question;
-    public TextMesh text;
 	// Use this for initialization
 	void Start () {
         nma.SetDestination(player.transform.position);
-        text.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
         nma.SetDestination(player.transform.position);
-        if (asked)
-        {
-            if (asked)
-            {
-                int answer = 0;
-                if (int.TryParse(Input.inputString, out answer))
-                {
-                    if (question.isRight(answer))
-                    {
-                        asked = false;
-                        unPauseGame();
-                        text.text = "";
-                        this.gameObject.SetActive(false);
-                    }
-                }
-            }
-        }
-    }
-    void OnCollisionEnter(Collision other)
-    {
-
-        if (other.collider.tag == "Player")
-        {
-            Time.timeScale = 0.0F;
-            question = new Question(1, 10);
-            text.text = question.getQuestion();
-            asked = true;
-        }
-    }
-    void unPauseGame()
-    {
-        Time.timeScale = 1.0F;
+        Debug.Log(nma.destination);
     }
 }
--- Assets/Scripts/SafePlayerPosition.cs
+++ Assets/Scripts/SafePlayerPosition.cs
@@ -5,15 +5,25 @@ public class SafePlayerPosition : MonoBehaviour {
     public GameObject player;
 	// Use this for initialization
 	void Start () {
-
+        if (IsSet())
+        {
+            player.transform.position = GetPlayerPosition();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+        PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
+        PlayerPrefs.SetFloat("PlayerZ", player.transform.position.z);
+        PlayerPrefs.SetFloat("isset", 1.0F);
     }
-    void Awaker()
+    public Vector3 GetPlayerPosition()
     {
-        DontDestroyOnLoad(player);
+        return new Vector3(PlayerPrefs.GetFloat("PlayerX"), PlayerPrefs.GetFloat("PlayerY"), PlayerPrefs.GetFloat("Playerz"));
+    }
+    public bool IsSet()
+    {
+        return PlayerPrefs.GetFloat("isset") == 1.0F;
     }
 }
